68c1d49e20c4e67705763af8a25027620ae0506b
ZacksEasyTeam_Account <165463731+EnterpriseExperience@users.noreply.github.com>
2025-01-11 08:05:19 -0500

    Update Reverse_Names.lua

--- Reverse_Names.lua
+++ Reverse_Names.lua
@@ -1,4 +1,7 @@
-local replicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
-for i,v in next, getupvalue(require(replicatedStorage.Modules.Network).fire, 3) do
-    v.Name = i
+if game.PlaceId == 12699642568 then
+    repeat wait() until game:IsLoaded() and game:GetService("ReplicatedStorage"):FindFirstChild("Events") and game:GetService("ReplicatedStorage"):FindFirstChild("Modules")
+    local replicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
+    for i,v in next, getupvalue(require(replicatedStorage.Modules.Network).fire, 3) do
+        v.Name = i
+    end
 end
